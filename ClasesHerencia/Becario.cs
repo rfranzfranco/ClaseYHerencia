dc0651695b35b3bf21dd7727398ce8cda0666b6e
Sergio Diaz <rfranz188@gmail.com>
2023-03-24 13:46:27 -0400

    clase becario agregada

---
+++ ClasesHerencia/Becario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHerencia
+{
+    internal class Becario : Usuario
+    {
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Tipo_beca { get; set; }
+        public DateTime f_inicio { get; set; }
+        public DateTime f_final { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
